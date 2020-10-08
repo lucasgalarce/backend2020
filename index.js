0c6a08d72097493145b6635381a938b193220a57
galarcelucas@gmail.com <galarcelucas@gmail.com>
2020-10-08 11:37:19 -0300

    refactor year

--- index.js
+++ index.js
@@ -39,20 +39,30 @@ const match = require('./server/models/match');
 
         // Carga de fechas
         // Cargo la primer fecha manual porque tiene un selector distinto
-
-        let year = dates[0].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.dataset.competitionMatchesList;
+        // function nthParent(element, n) {
+        //   while(n-- && element)
+        //     element = element.parentNode;
+        //   return element;
+        // }
+        // let year = nthParent(dates[0], 7) 
+        let year = dates[0].closest(".matches-list")
+        year = year.dataset.competitionMatchesList
+        // let year = dates[0].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.dataset.competitionMatchesList;
         year = year.split(" ")
         year = year[1]
         datesArray.push(`${dateLastMatch} ${year}`)
 
         dates.forEach((date, index) => {
           if(index >= 49) return;
-          let year = dates[index].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.dataset.competitionMatchesList;
+          // let year = nthParent(dates[index], 7) 
+          let year = dates[index].closest(".matches-list")
+          year = year.dataset.competitionMatchesList
+          // let year = dates[index].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.dataset.competitionMatchesList;
           year = year.split(" ")
           year = year[1]
           datesArray.push(`${date.innerText} ${year}`)
         });
-        
+
         // Carga de resultados, del local y del visitante
         // Hago la carta del resultado del ultimo partido ya que no tiene el mismo selector que los demas
         localResultsArray.push(resLastMatch[0])
@@ -77,15 +87,15 @@ const match = require('./server/models/match');
           })
         }
         
-        return match
+        return datesArray
     })
 
-    matchs.forEach((element, index) => {
-        element.date = new Date(element.date)
-    });
+    // matchs.forEach((element, index) => {
+    //     element.date = new Date(element.date)
+    // });
 
     console.log(matchs)
-    console.log(matchs.length)
+    // console.log(matchs.length)
 
   await browser.close();
 })();
