06479f354c4bbdc2f47d84d02e906fcaab4b2b6b
galarcelucas@gmail.com <galarcelucas@gmail.com>
2020-10-09 13:26:53 -0300

    axios, cron and config initial request

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ process.env.PORT = process.env.PORT || 3000;
 // 60 minutos
 // 24 horas
 // 30 días
-process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+process.env.CADUCIDAD_TOKEN = '30d'
 
 // SEED de autenticación
 process.env.SEED = process.env.SEED || 'dev-seed';
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-require('./config/config')
+require('./config/config');
 
 const express = require("express");
 const mongoose = require("mongoose");
+const axios = require('axios')
+const cron = require('node-cron');
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -27,6 +29,88 @@ mongoose.connect(process.env.URLDB, {
 
     console.log("DB online");
 });
+
+// Funcion que se ejecuta al iniciar el servidor, crea un usuario, logea y consigue token, y hace el request para cargar los partidos
+(async () => {
+  const email = 'emailtest@gmail.com';
+  const password = '1234';
+  let token;
+  // Creo un usuario
+  await axios.post('http://localhost:3000/user/', {
+    email,
+    password
+  }).then( (response) => {
+    console.log(response.data)
+  }).catch( (err) => {
+    console.log(err.response.data)
+  })
+
+  // Logueo y consigo token
+  await axios.post('http://localhost:3000/login/', {
+    email,
+    password
+  }).then( (response) => {
+    console.log(response.data)
+    token = response.data.token
+  }).catch( (err) => {
+    console.log(err.response.data)
+  })
+
+  // Hago el request con el token
+  console.log("Adding matches...")
+  await axios.post('http://localhost:3000/loadmatches', {}, {
+    headers: {
+      'token': token
+    }
+  }).then( (response) => {
+    console.log(response.data)
+  }).catch( (err) => {
+    console.log(err)
+  })
+  console.log("Added matches")
+})();
+
+// Cron programado para correr todos los dias a las 00hs, lo dejo comentado porque opte por la opcion de que se 
+// carguen los datos al iniciar el server
+// cron.schedule('0 0 * * *', async function() {
+//   console.log('Running task...');
+//   const email = 'emailtest@gmail.com';
+//   const password = '1234';
+//   let token;
+//   // Creo un usuario
+//   await axios.post('http://localhost:3000/user/', {
+//     email,
+//     password
+//   }).then( (response) => {
+//     console.log(response.data)
+//   }).catch( (err) => {
+//     console.log(err.response.data)
+//   })
+
+//   // Logueo y consigo token
+//   await axios.post('http://localhost:3000/login/', {
+//     email,
+//     password
+//   }).then( (response) => {
+//     console.log(response.data)
+//     token = response.data.token
+//   }).catch( (err) => {
+//     console.log(err.response.data)
+//   })
+
+//   // Hago el request con el token
+//   console.log("Adding matches...")
+//   await axios.post('http://localhost:3000/loadmatches', {}, {
+//     headers: {
+//       'token': token
+//     }
+//   }).then( (response) => {
+//     console.log(response.data)
+//   }).catch( (err) => {
+//     console.log(err)
+//   })
+//   console.log("Added matches")
+// });
 
 app.listen(PORT, () => {
     console.log(`Listening port ${PORT}`)
